df08ea5f3f547428f197c27b04b60b233e59a581
eri-amm <ermh8998@gmail.com>
2024-09-15 04:50:19 +0330

    added score tracking, medals and info panel

--- Assets/Scripts/Gameplay/Logics/logics.cs
+++ Assets/Scripts/Gameplay/Logics/logics.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,17 +8,19 @@ using UnityEngine.UI;
 public class logics : MonoBehaviour
 {
     public int score=0;
-    public Text scoreT;
+    public Text scoreT,highScore,boardScore,boardHigh;
     public AudioSource point,ost;
     public Button pauseB;
-    public Sprite pauseS, playS;
-    public GameObject gameOvers,startMenu,gameplay,scoreTx;
+    public Image medal;
+    public Sprite pauseS, playS,bronze,silver,gold,plat;
+    public GameObject gameOvers,startMenu,gameplay,scoreTx,newHigh,Medal,flappy;
 
     void Start()
     {
         point.volume = PlayerPrefs.GetInt("PointsVol");
         ost.volume = PlayerPrefs.GetInt("MusicVol");
         ost.Play();
+        highScore.text = Convert.ToString(PlayerPrefs.GetInt("HighScore"));
     }
     public void inc_scr()
     {
@@ -33,10 +36,12 @@ public class logics : MonoBehaviour
     }
     public void pause()
     {
+
         if (Time.timeScale != 0)
         {
             Time.timeScale = 0;
             pauseB.image.overrideSprite = playS;
+            
         }
         else
         {
@@ -53,6 +58,40 @@ public class logics : MonoBehaviour
     }
     public void gameOver()
     {
+        if (score > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            newHigh.SetActive(true);
+        }
+        boardScore.text = score.ToString();
+        boardHigh.text = PlayerPrefs.GetInt("HighScore").ToString();
         gameOvers.SetActive(true);
+        gameplay.SetActive(false);
+        scoreTx.SetActive(false );
+        if (score >= 10)
+        {
+            Medal.SetActive(true );    
+        }
+
+        if (score >= 10&&score<20)
+        {
+            medal.overrideSprite = bronze;
+            medal.rectTransform.sizeDelta = new Vector2(383.4f, 341.6f);
+        }
+        else if (score >= 20 && score < 30)
+        {
+            medal.overrideSprite = silver;
+            medal.rectTransform.sizeDelta = new Vector2(359.3f, 317.1f);
+        }
+        else if (score >= 1 && score < 40)
+        {
+            medal.overrideSprite = gold;
+            medal.rectTransform.sizeDelta = new Vector2(366.1f, 326.1f);
+        }
+        else if (score > 40)
+        {
+            medal.overrideSprite = plat;
+            medal.rectTransform.sizeDelta = new Vector2(350.9f, 338.5f);
+        }
     }
 }
--- Assets/Scripts/Settings/scene_managerr.cs
+++ Assets/Scripts/Settings/scene_managerr.cs
@@ -6,8 +6,20 @@ using UnityEngine.UI;
 
 public class scene_managerr : MonoBehaviour
 {
+    public GameObject panel,menu;
     public void change(string Scene_name)
     {
         SceneManager.LoadScene(Scene_name);
+    }
+    public void panel_Open()
+    {
+        panel.SetActive(true);
+        menu.SetActive(false);
+    }
+
+    public void panel_close()
+    {
+        panel.SetActive(false);
+        menu.SetActive(true);
     }
 }
